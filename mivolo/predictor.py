b0dc6a2159d4fad2042ec3aaadeefbb637d6f204
Devin Ha <t.ha@tu-berlin.de>
2024-10-07 17:57:03 +0200

    add util functions to predictor

--- mivolo/predictor.py
+++ mivolo/predictor.py
@@ -7,11 +7,14 @@ import tqdm
 from mivolo.model.mi_volo import MiVOLO
 from mivolo.model.yolo_detector import Detector
 from mivolo.structures import AGE_GENDER_TYPE, PersonAndFaceResult
+import torch
 
 
 class Predictor:
     def __init__(self, config, verbose: bool = False):
-        self.detector = Detector(config.detector_weights, config.device, verbose=verbose)
+        self.detector = Detector(
+            config.detector_weights, config.device, verbose=verbose
+        )
         self.age_gender_model = MiVOLO(
             config.checkpoint,
             config.device,
@@ -22,7 +25,9 @@ class Predictor:
         )
         self.draw = config.draw
 
-    def recognize(self, image: np.ndarray) -> Tuple[PersonAndFaceResult, Optional[np.ndarray]]:
+    def recognize(
+        self, image: np.ndarray
+    ) -> Tuple[PersonAndFaceResult, Optional[np.ndarray]]:
         detected_objects: PersonAndFaceResult = self.detector.predict(image)
         self.age_gender_model.predict(image, detected_objects)
 
@@ -66,3 +71,83 @@ class Predictor:
             if self.draw:
                 frame = detected_objects.plot()
             yield detected_objects_history, frame
+
+    def prepare_input(self, img_x):
+        detected_bboxes = self.detector.predict(img_x)
+        if (
+            (detected_bboxes.n_objects == 0)
+            or (
+                not self.age_gender_model.meta.use_persons
+                and detected_bboxes.n_faces == 0
+            )
+            or (
+                self.age_gender_model.meta.disable_faces
+                and detected_bboxes.n_persons == 0
+            )
+        ):
+            # nothing to process
+            print("Nothing detected. Nothing to process")
+            return
+
+        (
+            faces_input,
+            person_input,
+            faces_inds,
+            bodies_inds,
+        ) = self.age_gender_model.prepare_crops(img_x, detected_bboxes)
+        return faces_input, person_input
+
+    def inference_grads(
+        self, faces_input: torch.Tensor, person_input: torch.Tensor = None
+    ):
+        if faces_input is None:
+            print("No face found. Nothing to process")
+            return
+
+        if self.age_gender_model.meta.with_persons_model and person_input is None:
+            print("No person found. Nothing to process")
+            return
+
+        if (
+            self.age_gender_model.meta.with_persons_model
+        ):  # Disabled for face only model
+            model_input = torch.cat((faces_input, person_input), dim=1)
+        else:
+            model_input = faces_input
+
+        model_input.requires_grad_()
+
+        if self.age_gender_model.half:
+            model_input = model_input.half()
+
+        output = self.age_gender_model.model(model_input)
+
+        age = (
+            output
+            * (self.age_gender_model.meta.max_age - self.age_gender_model.meta.min_age)
+            + self.age_gender_model.meta.avg_age
+        )
+
+        return age
+
+    def denormalize(self, x):
+        # Image is in BGR Format
+        device = x.device
+        mean = (
+            torch.Tensor(self.age_gender_model.data_config["mean"])
+            .view(1, 3, 1, 1)
+            .to(device)
+        )
+        std = (
+            torch.Tensor(self.age_gender_model.data_config["std"])
+            .view(1, 3, 1, 1)
+            .to(device)
+        )
+        x_denormed = x * std + mean
+        x_denormed = x_denormed.clamp(0, 1).permute(0, 2, 3, 1).cpu().numpy()
+
+        # Convert to RGB
+        for i in range(x_denormed.shape[0]):
+            x_denormed[i] = cv2.cvtColor(x_denormed[i], cv2.COLOR_BGR2RGB)
+
+        return x_denormed
